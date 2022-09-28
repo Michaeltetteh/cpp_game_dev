062cde13fbf44fe0b5a4bb7c928eeaa0c8e3d637
mike <miketetteh751@outlook.com>
2022-09-28 17:25:55 +0000

    refactored

---
+++ sdl/cmake/common.cmake
@@ -0,0 +1,5 @@
+find_package(SDL2  REQUIRED)
+include_directories(${SDL2_INCLUDE_DIRS})
+
+set(SDL_IMAGE_INCLUDE "/usr/local/Cellar/sdl2_image/2.6.2/include/" CACHE PATH "path to sdl_image" ) 
+set(SDL_IMAGE_LIBRARY "/usr/local/Cellar/sdl2_image/2.6.2/lib/libSDL2_image-2.0.0.dylib" CACHE FILEPATH "sdl_image lib path" )
